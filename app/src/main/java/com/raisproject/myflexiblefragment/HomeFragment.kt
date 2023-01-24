a421ecf7149e563815fd509c4b1d51b78c7a6656
raismuhammad <muhammadrais96@gmail.com>
2023-01-24 11:13:31 +0700

    Latihan Fragment di Satu Activity

--- app/src/main/java/com/raisproject/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/raisproject/myflexiblefragment/HomeFragment.kt
@@ -28,6 +28,14 @@ class HomeFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        // do nothing
+        if (v?.id == R.id.btn_category) {
+            val mCategoryFragment = CategoryFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
